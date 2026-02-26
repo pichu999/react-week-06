2c75d9cd4b730b53f3639b48575de1c5179592d4
yuju <yuju99924@gmail.com>
2026-02-26 19:06:30 +0800

    feat:add form validation using react-hook-form

--- src/views/front/Checkout.jsx
+++ src/views/front/Checkout.jsx
@@ -1,11 +1,19 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 
 const API_BASE = import.meta.env.VITE_API_BASE;
 const API_PATH = import.meta.env.VITE_API_PATH;
 
 function Checkout() {
   const [cart, setCart] = useState();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ mode: "onChange" });
+
   const getCart = async () => {
     try {
       const res = await axios.get(`${API_BASE}/api/${API_PATH}/cart`);
@@ -49,6 +57,22 @@ function Checkout() {
       getCart();
     } catch {
       alert("刪除產品失敗");
+    }
+  };
+
+  const onSubmit = async (formData) => {
+    try {
+      const data = {
+        user: formData,
+        message: formData.message,
+      };
+      const res = await axios.post(`${API_BASE}/api/${API_PATH}/order`, {
+        data,
+      });
+      const res2 = await axios.get(`${API_BASE}/api/${API_PATH}/cart`);
+      setCart(res2.data.data);
+    } catch {
+      alert("訂單送出失敗");
     }
   };
 
@@ -120,6 +144,118 @@ function Checkout() {
           </tr>
         </tfoot>
       </table>
+      {/* 結帳頁面 */}
+      <div className="my-5 row justify-content-center">
+        <form className="col-md-6" onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              className="form-control"
+              placeholder="請輸入 Email"
+              {...register("email", {
+                required: "請輸入 Email",
+                pattern: {
+                  value: /^\S+@\S+$/i,
+                  message: "Email 格式不正確",
+                },
+              })}
+            />
+            {errors.email && (
+              <p className="text-danger">{errors.email.message}</p>
+            )}
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label">
+              收件人姓名
+            </label>
+            <input
+              id="name"
+              name="name"
+              type="text"
+              className="form-control"
+              placeholder="請輸入姓名"
+              {...register("name", {
+                required: "請輸入收件人姓名",
+                minLength: {
+                  value: 2,
+                  message: "姓名至少 2 個字",
+                },
+              })}
+            />
+            {errors.name && (
+              <p className="text-danger">{errors.name.message}</p>
+            )}
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="tel" className="form-label">
+              收件人電話
+            </label>
+            <input
+              id="tel"
+              name="tel"
+              type="tel"
+              className="form-control"
+              placeholder="請輸入電話"
+              {...register("tel", {
+                required: "請輸入收件人電話",
+                minLength: {
+                  value: 8,
+                  message: "電話至少 8 碼",
+                },
+                pattern: {
+                  value: /^\d+$/,
+                  message: "電話僅能輸入數字",
+                },
+              })}
+            />
+            {errors.tel && <p className="text-danger">{errors.tel.message}</p>}
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="address" className="form-label">
+              收件人地址
+            </label>
+            <input
+              id="address"
+              name="address"
+              type="text"
+              className="form-control"
+              placeholder="請輸入地址"
+              {...register("address", {
+                required: "請輸入收件人地址",
+              })}
+            />
+            {errors.address && (
+              <p className="text-danger">{errors.address.message}</p>
+            )}
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="message" className="form-label">
+              留言
+            </label>
+            <textarea
+              id="message"
+              className="form-control"
+              cols="30"
+              rows="10"
+              {...register("message")}
+            ></textarea>
+          </div>
+          <div className="text-end">
+            <button type="submit" className="btn btn-danger">
+              送出訂單
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
