1a882ba83b72ca47fa302afcc75dc64bbd14092a
yuju <yuju99924@gmail.com>
2026-02-26 16:39:12 +0800

    feat:add Checkout and Login page

--- src/layout/FrontendLayout.jsx
+++ src/layout/FrontendLayout.jsx
@@ -21,6 +21,16 @@ function FrontendLayout() {
                 購物車
               </Link>
             </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/checkout">
+                結帳
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">
+                登入
+              </Link>
+            </li>
           </ul>
         </div>
       </header>
